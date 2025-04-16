124ad738e0776bc688679da35e8ec50271c09dcd
Zach-Adolphsen <zladolphsen@gmail.com>
2025-04-16 10:05:57 -0500

    injected Firestore in UserService

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,18 +1,25 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
-
-
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 // Your web app's Firebase configuration firebaseConfig goes here
 // Copy the firebaseConfig object from your Firebase project settings
-// const firebaseConfig = { ...... }
-
+const firebaseConfig = {
+  apiKey: 'AIzaSyCoO2HsTsH5HCttkThCGji-jXpBWjmInPo',
+  authDomain: 'practice-users.firebaseapp.com',
+  projectId: 'practice-users',
+  storageBucket: 'practice-users.firebasestorage.app',
+  messagingSenderId: '737173097374',
+  appId: '1:737173097374:web:2718c586c0e049c924e48f',
+};
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-  
-  ]
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+  ],
 };
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,48 +1,64 @@
-import {Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
+import {
+  collection,
+  collectionData,
+  deleteDoc,
+  doc,
+  Firestore,
+  setDoc,
+  updateDoc,
+} from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
-
-export interface User{
-  id: string,
-  name: string,
-  email: string
+export interface User {
+  id: string;
+  name: string;
+  email: string;
 }
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-
   // This service is for CRUD operations on users
   // It uses AngularFire for Firestore operations
   // It uses RxJS for observables
 
-  
   // Injecting Firestore service from AngularFire
-  // Firestore is a NoSQL database from Firebase    
- 
+  // Firestore is a NoSQL database from Firebase
+  private firestore = inject(Firestore);
+  private userCollection = collection(this.firestore, 'users');
 
   // It uses AngularFire's collection function to get a reference to the users collection
-
+  getUsers(): Observable<User[]> {
+    return collectionData(this.userCollection, { idField: 'id' }) as Observable<
+      User[]
+    >;
+  }
 
   // It uses AngularFire's collectionData function to get the data from the users collection
   // It uses RxJS's Observable to return the data as an observable
 
-
   // It uses AngularFire's setDoc function to add a new user to the users collection
   // It uses AngularFire's doc function to get a reference to the new user document
 
+  addUser(newUser: User) {
+    const userRef = doc(this.firestore, `users/${newUser.id}`);
+    newUser.id = userRef.id;
+    setDoc(userRef, newUser);
+  }
 
   // It uses AngularFire's updateDoc function to update an existing user in the users collection
   // It uses AngularFire's doc function to get a reference to the user document
-  // updateUser(user:User){
-  //   const userRef = doc(this.firestore, `users/${user.id}`);
-  //   updateDoc(userRef,{ ... user})
-  // }
+  updateUser(user: User) {
+    const userRef = doc(this.firestore, `users/${user.id}`);
+    updateDoc(userRef, { ...user });
+  }
 
   // It uses AngularFire's deleteDoc function to delete a user from the users collection
   // It uses AngularFire's doc function to get a reference to the user document
-  // deleteUser(id:string){
-  //   const userRef = doc(this.firestore, `users/${id}`);
-  //   deleteDoc(userRef);
-  // }
+  deleteUser(id: string) {
+    const userRef = doc(this.firestore, `users/${id}`);
+    deleteDoc(userRef);
+  }
 }
